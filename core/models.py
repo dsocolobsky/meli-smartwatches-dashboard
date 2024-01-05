0204cff9a2f5f33832570b772279933294422f7c
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-01-05 19:07:29 -0300

    Add a cache and use it for most expensive items

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 
-# Create your models here.
+
+class CacheData(models.Model):
+    # This controls for how many minutes the cache is valid
+    most_expensive_cache_minutes = models.IntegerField()
+    vendor_data_cache_minutes = models.IntegerField()
+    # Last timestamp when the cache was updated
+    most_expensive_last_update = models.DateTimeField()
+    vendor_data_last_update = models.DateTimeField()
+
+    def is_most_expensive_cache_valid(self):
+        return (
+            self.most_expensive_last_update
+            + timedelta(minutes=self.most_expensive_cache_minutes)
+            > datetime.now()
+        )
+
+    def is_vendor_data_cache_valid(self):
+        return (
+            self.vendor_data_last_update
+            + timedelta(minutes=self.vendor_data_cache_minutes)
+            > datetime.now()
+        )
+
+
+class MeliItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    permalink = models.URLField()
--- core/views.py
+++ core/views.py
@@ -1,16 +1,47 @@
 import django
 from django.shortcuts import render
 
+from core import models
 from core.services.meli import MeliService
 
 
 class HomeView(django.views.View):
     def get(self, request):
+        cache = models.CacheData.objects.first()
+        if cache and cache.is_most_expensive_cache_valid():
+            print("Cache is valid, using")
+            items = models.MeliItem.objects.all()
+            return render(
+                request,
+                "mas_caros.html",
+                {"items": items, "last_update": cache.most_expensive_last_update},
+            )
+
+        print("Cache is invalid, fetching")
         try:
             items = MeliService().fetch_most_expensives("MLA352679", 50)
         except Exception:
             return render(request, "error.html")
-        return render(request, "mas_caros.html", {"items": items})
+        # Delete all old data and persist newly fetched items
+        models.MeliItem.objects.all().delete()
+        models.MeliItem.objects.bulk_create(
+            [
+                models.MeliItem(
+                    title=item["title"],
+                    price=item["price"],
+                    permalink=item["permalink"],
+                )
+                for item in items
+            ]
+        )
+        # Update the cache timestamp
+        cache.most_expensive_last_update = django.utils.timezone.now()
+        cache.save()
+        return render(
+            request,
+            "mas_caros.html",
+            {"items": items, "last_update": cache.most_expensive_last_update},
+        )
 
 
 class SellerStatsView(django.views.View):
