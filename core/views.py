c2cde5317bc6acc8babcc2cf74613e7e662d4eef
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-01-05 18:05:23 -0300

    refactor: move fetch/business code into meli.py. Also add error page.

--- core/views.py
+++ core/views.py
@@ -2,18 +2,16 @@ import django
 import requests
 from django.shortcuts import render
 
+from core.services.meli import MeliService
+
+
 class HomeView(django.views.View):
 
     def get(self, request):
-        meli_response = requests.get('https://api.mercadolibre.com/sites/MLA/search/',
-                         params={'category': 'MLA352679', 'limit': '10', 'sort': 'price_desc'}).json()
-
-        print(meli_response)
-        items = [{'title': item['title'], 'price': item['price'], 'permalink': item['permalink']}
-                 for item in meli_response['results']]
-
-        print(items)
-
+        try:
+            items = MeliService().fetch_most_expensives('MLA352679', 50)
+        except Exception:
+            return render(request, 'error.html')
         return render(request, 'mas_caros.html', {'items': items})
 
 
@@ -21,40 +19,15 @@ class SellerStatsView(django.views.View):
 
     def get(self, request):
         # Get the first 1000 items from the Smartwatch category
-        meli_response = requests.get('https://api.mercadolibre.com/sites/MLA/search/',
-                         params={'category': 'MLA352679', 'limit': '50'}).json()
-        items = meli_response['results']
-        print(f"Total items: {len(items)}")
-        sellers = set([item['seller']['id'] for item in items][:4])
-
+        meli = MeliService()
+        try:
+            vendor_ids = meli.fetch_vendors_from_category('MLA352679', 50)
+        except Exception:
+            return render(request, 'error.html')
         data = []
-        for seller in sellers:
-            print(f"Getting data for seller {seller}")
-            data.append(self.data_for_seller(seller))
+        for vendor_id in vendor_ids:
+            data.append(meli.fetch_vendor_data(vendor_id))
         # Sort by total items desc
         data.sort(key=lambda x: x['total_items'], reverse=True)
 
         return render(request, 'vendedores.html', {'sellers': data})
-
-    def data_for_seller(self, seller):
-        data = {}
-        meli_response = requests.get('https://api.mercadolibre.com/sites/MLA/search/',
-                                     params={'seller_id': seller, 'limit': '50'}).json()
-
-        data['seller_id'] = seller
-        data['seller_name'] = meli_response['seller']['nickname']
-        data['total_items'] = meli_response['paging']['total']
-        data['gold_special'] = 0
-        data['gold_pro'] = 0
-        total_price = 0
-        for item in meli_response['results']:
-            print(item)
-            total_price += item['price']
-            listing_type_id = item.get('listing_type_id', None)
-            if listing_type_id == 'gold_special':
-                data['gold_special'] += 1
-            elif listing_type_id == 'gold_pro':
-                data['gold_pro'] += 1
-
-        data['avg_price'] = total_price / data['total_items']
-        return data
