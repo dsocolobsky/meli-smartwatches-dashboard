d9bdadb1c9798bceff27c28ed3b4bd26275555b7
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-01-05 16:35:41 -0300

    very basic home that shows more expensive watches

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
+import django
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+class HomeView(django.views.View):
+
+    def get(self, request):
+        meli_response = requests.get('https://api.mercadolibre.com/sites/MLA/search/',
+                         params={'category': 'MLA352679', 'limit': '10', 'sort': 'price_desc'}).json()
+
+        print(meli_response)
+        items = [{'title': item['title'], 'price': item['price'], 'permalink': item['permalink']}
+                 for item in meli_response['results']]
+
+        print(items)
+
+        return render(request, 'mas_caros.html', {'items': items})
